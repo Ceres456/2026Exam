8a03877981d55797feae946fdd31de67591dab5f
wodyd456 <wodyd2131@gmail.com>
2026-03-31 11:38:17 +0900

    5장 요청처리 메소드의 파라미터 유형

---
+++ src/main/java/kr/ac/kopo/wodyd/_026exam/controller/Chap05_02Controller.java
@@ -0,0 +1,28 @@
+package kr.ac.kopo.wodyd._026exam.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Calendar;
+
+@Controller
+public class Chap05_02Controller {
+
+    @GetMapping("/chap0502/{name}/{birthyear}")
+    public String requestMethod(@PathVariable("name") String name, @PathVariable("birthyear") int birthyear, Model model){
+        Calendar calendar = Calendar.getInstance();
+        int nowYear = calendar.get(Calendar.YEAR);
+        int age = nowYear - birthyear;
+
+        model.addAttribute("name", name);
+        model.addAttribute("age", age);
+        return "viewPage05_02";
+    }
+
+
+
+
+}
