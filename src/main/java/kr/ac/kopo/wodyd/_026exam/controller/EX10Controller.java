4f32238cbe960ce17918808bdc4f700bfac4c741
wodyd456 <wodyd2131@gmail.com>
2026-05-12 09:46:30 +0900

    파일 전송을 배웠따!2

---
+++ src/main/java/kr/ac/kopo/wodyd/_026exam/controller/EX10Controller.java
@@ -0,0 +1,8 @@
+package kr.ac.kopo.wodyd._026exam.controller;
+
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class EX10Controller {
+}
--- src/main/java/kr/ac/kopo/wodyd/_026exam/domain/Member.java
+++ src/main/java/kr/ac/kopo/wodyd/_026exam/domain/Member.java
@@ -2,15 +2,18 @@ package kr.ac.kopo.wodyd._026exam.domain;
 
 import lombok.Data;
 import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
 
 @Data
 public class Member {
-    private String id;
-    private String passwd;
-    private String city;
-    private String sex;
-    private String[] hobby;
-    private String greeting;
+//    private String id;
+//    private String passwd;
+//    private String city;
+//    private String sex;
+//    private String[] hobby;
+//    private String greeting;
+    private String name;
+    private MultipartFile fileImage;
 
 
 
