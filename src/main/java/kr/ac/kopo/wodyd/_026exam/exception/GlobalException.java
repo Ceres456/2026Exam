b4f5eb9d92916a484e4c66e06d7c50edfb8fb130
wodyd456 <wodyd2131@gmail.com>
2026-08-18 10:58:13 +0900

    3. @ControllerAdvice 와 @ExceptionHandler를 사용한 예외 처리 방법

---
+++ src/main/java/kr/ac/kopo/wodyd/_026exam/exception/GlobalException.java
@@ -0,0 +1,8 @@
+package kr.ac.kopo.wodyd._026exam.exception;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice(basePackages = {"com.springboot"})
+public class GlobalException {
+}
